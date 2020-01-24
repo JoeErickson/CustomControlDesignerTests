984093cb3f1f5c0d0e3fd4bb16cd8a84d0dce9cb
Chris Hamilton <chris@tracktownsoftware.com>
2020-01-24 12:19:47 -0800

    clean up

--- WpfControlNetFramework.Design/RegisterMetaData.cs
+++ WpfControlNetFramework.Design/RegisterMetaData.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using Microsoft.Windows.Design;
+using Microsoft.Windows.Design.Features;
+using Microsoft.Windows.Design.Metadata;
+
+// The ProvideMetadata assembly-level attribute indicates to designers
+// that this assembly contains a class that provides an attribute table. 
+[assembly: ProvideMetadata(typeof(WpfControlNetFramework.Design.RegisterMetaData))]
 
 namespace WpfControlNetFramework.Design
 {
-    public class RegisterMetaData
+    public class RegisterMetaData : IProvideAttributeTable
     {
+        public AttributeTable AttributeTable
+        {
+            get
+            {
+                AttributeTableBuilder builder = new AttributeTableBuilder();
+
+                // Add the menu provider to the design-time metadata.
+                builder.AddCustomAttributes(
+                    typeof(WpfControlNetFramework.ButtonWithDesignTime),
+                    new FeatureAttribute(typeof(CustomContextMenuProvider)));
+
+                return builder.CreateTable();
+            }
+        }
     }
 }
+
